10f4f5384b9b1ff0ec207c286d9c8191979fde71
Vincent <vincent.glize@live.fr>
2017-03-11 23:14:43 +0100

    Update Copyright

--- src/GameBundle/Model/MapManager.php
+++ src/GameBundle/Model/MapManager.php
@@ -1,5 +1,14 @@
 <?php
 /**
+ * This file is part of the World Of Shinobi (Minegame).
+ *
+ * Copyright (c) 2017. Vincent Glize <vincent.glize@live.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
  * Created by PhpStorm.
  * User: vincent
  * Date: 17/11/16
--- src/GameBundle/Model/RessourceManager.php
+++ src/GameBundle/Model/RessourceManager.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file is part of the World Of Shinobi (Minegame).
+ *
+ * Copyright (c) 2017. Vincent Glize <vincent.glize@live.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace GameBundle\Model;
 
 use Doctrine\ORM\EntityManager;
